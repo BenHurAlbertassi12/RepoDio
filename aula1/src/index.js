1def63e4da8d1b396e66204e7659662124dc42fe
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-15 19:13:09 -0300

    configurei errado, só assisti a aula

--- aula1/src/index.js
+++ aula1/src/index.js
@@ -1,42 +1,39 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-// import Button from './Button'
+import Button from './Button'
 import './index.css';
 
-// const soma = (a, b) => a + b
 
 class App extends Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.state = {
-      clock: 1000,
-      copo: 'agua'
+        this.state = {
+            clock: 1000,
+            copo: 'agua'
+        }
     }
-  }
 
-  componentDidMount() {
-    window.setTimeout(() => {
-      this.setState({
-        copo: 'suco'
-      })
-    }, 3000)
-  }
+    componentDidMount() {
+        this.setState({
+            copo: 'suco'
+        })
+    }
 
-  alterarCopo = () => {
-    this.setState({
-      copo: 'refrigerante'
-    })
-  }
+    alterarCopo = () => {
+        this.setState({
+            copo: 'refrigerante'
+        })
+    }
 
-  render() {
-    return (
-      <div>
-        <h1>{this.state.clock}</h1>
-        <button onClick={() => this.alterarCopo()}>{this.state.copo}</button>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.clock}</h1>
+                <button onClick={() => this.alterarCopo()}>{this.state.copo}</button>
+            </div>
+        )
+    }
 }
 
 const rootElement = document.getElementById('root')
