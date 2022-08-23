5b34ad1877eff31cf6b76a1f16305e6cc6702337
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-23 19:19:36 -0300

    First PR

---
+++ bagulhos-estranhos/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Inicial from './components/Inicial';
+
+function App() {
+  return (
+    <div className="App">
+ <Inicial />
+    </div>
+  );
+}
+
+export default App;
