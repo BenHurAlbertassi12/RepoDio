6d736b0a2001dd7a80d1a9f20015b59a037cebc0
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-01-06 15:49:34 -0300

    Organização

--- ClassAnatony/src/edu/benhur/anatomiaclasses/BoletimEstudantil.java
+++ ClassAnatony/src/edu/benhur/anatomiaclasses/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package edu.benhur.anatomiaclasses;
 public class BoletimEstudantil {
     public static void main(String[] args) {
         int mediaFinal = 6;
--- ClassAnatony/src/edu/benhur/anatomiaclasses/MinhaClasse.java
+++ ClassAnatony/src/edu/benhur/anatomiaclasses/MinhaClasse.java
@@ -1,3 +1,4 @@
+package edu.benhur.anatomiaclasses;
 public class MinhaClasse {
     
     public static void main(String[] args) {
