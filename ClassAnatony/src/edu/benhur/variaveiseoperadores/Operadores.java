4d4fd93dfa7217b63da947cd3a536f89b56b90c1
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-01-06 17:02:30 -0300

    Relacionais

--- ClassAnatony/src/edu/benhur/variaveiseoperadores/Operadores.java
+++ ClassAnatony/src/edu/benhur/variaveiseoperadores/Operadores.java
@@ -68,6 +68,22 @@ public class Operadores {
         
         int resultado2 = c == d ? 1 : 0;
         System.out.println(resultado2);
+        
+        int numero1 = 1;
+        int numero2 = 2;
+        
+        boolean simNao = numero1 == numero2;
+        System.out.println("numero1 é igual a numero2? " + simNao);
+        
+        simNao = numero1 != numero2;
+        System.out.println("numero1 é igual a numero2? " + simNao);
+
+        //para usar objetos usar equals
+
+        String nomeUm = "Ben";
+        String nomeDois = "Ben";
+
+        System.out.println(nomeUm.equals(nomeDois));
     }
 
 }
