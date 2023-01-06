3f6e177dd6e481e0855dae3b01b1b249b7bcbc89
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-01-06 15:20:21 -0300

    Variaveis

---
+++ ClassAnatony/src/MinhaClasse.java
@@ -0,0 +1,12 @@
+public class MinhaClasse {
+    
+public static void main(String[] args) {
+        String meuNome = "BenHur Albertassi";
+
+        int anoFabricacao = 2019;
+
+        boolean vardadeiro = false;
+
+        anoFabricacao = 1989;
+    }
+}
