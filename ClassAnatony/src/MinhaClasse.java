4bca7f6a0b72c776f9b78624d975b39702b12846
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-01-06 15:28:26 -0300

    Dia 1 aula 2

--- ClassAnatony/src/MinhaClasse.java
+++ ClassAnatony/src/MinhaClasse.java
@@ -1,12 +1,16 @@
 public class MinhaClasse {
     
-public static void main(String[] args) {
-        String meuNome = "BenHur Albertassi";
+    public static void main(String[] args) {
+        String primeiroNome = "BenHur";
 
-        int anoFabricacao = 2019;
+        String segundoNome = "Albertassi";
 
-        boolean vardadeiro = false;
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
 
-        anoFabricacao = 1989;
+        System.out.println(nomeCompleto);
+    }
+    
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
