093e0ee603f3c46d3355ab7deed369a3c920ee1e
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-01-07 12:31:56 -0300

    If longo

---
+++ controlesdefluxo/src/edu/benhur/projetofluxo/ControleDeFluxo.java
@@ -0,0 +1,92 @@
+package edu.benhur.projetofluxo;
+
+public class ControleDeFluxo {
+    public static void main(String[] args) {
+
+        ifArrow();
+
+        ifNoArrow();
+
+        bigIf();
+
+        minumIf();
+
+        switchSemana();
+
+        switchNumero();
+
+        switchFerias();
+    }
+
+    public static void ifArrow() {
+        int mes = 9;
+        if (mes == 1) {
+            System.out.println("Janeiro");
+        } else {
+            if (mes == 2) {
+                System.out.println("Fevereiro");
+            } else {
+                if (mes == 3) {
+                    System.out.println("Março");
+                } else {
+                    if (mes == 4) {
+                        System.out.println("Abril");
+                    } else {
+                        if (mes == 5) {
+                            System.out.println("Mail");
+                        } else {
+                            if (mes == 6) {
+                                System.out.println("Junho");
+                            } else {
+                                if (mes == 7) {
+                                    System.out.println("Julho");
+                                } else {
+                                    if (mes == 8) {
+                                        System.out.println("Agosto");
+                                    } else {
+                                        if (mes == 9) {
+                                            System.out.println("Setembro");
+                                        } else {
+                                            if (mes == 10) {
+                                                System.out.println("Outubro");
+                                            } else {
+                                                if (mes == 11) {
+                                                    System.out.println("Novembro");
+                                                } else {
+                                                    if (mes == 12) {
+                                                        System.out.println("Dazembro");
+                                                    } else {
+
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    public static void ifNoArrow() {
+        
+    }
+    public static void minumIf() {
+        
+    }
+    public static void bigIf() {
+        
+    }
+    public static void switchSemana() {
+        
+    }
+    public static void switchNumero() {
+        
+    }
+    public static void switchFerias() {
+        
+    }
+}
