03948e5ced8d2d1e4b06c4dfe63ca1afe8e9bbcf
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-01-09 19:35:27 -0300

    Calculadora e Emprestimo

--- metodoscargaretorno/src/edu/dio/metodos/Mensagem.java
+++ metodoscargaretorno/src/edu/dio/metodos/Mensagem.java
@@ -4,37 +4,4 @@ import java.util.Scanner;
 
 public class Mensagem {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        int number1;
-        int number2;
-        String operator = "";
-
-        System.out.println("Escolha o primeiro número: ");
-        number1 = scan.nextInt();
-        
-        System.out.print("Escolha o operador: ");
-        operator = scan.next();
-        
-        System.out.println("Escolha o segundo número: ");
-        number2 = scan.nextInt();
-        
-        if(operator.equals("+")){
-            System.out.println("O Resultado da operação é: " + (number1 + number2));      
-        }
-        
-        if (operator.equals("-")){
-            System.out.println("O Resultado da operação é: " + (number1 - number2));                
-        }
-        
-        if (operator.equals("/")){
-            System.out.println("O Resultado da operação é: " + (number1 / number2));           
-        }
-        
-        if (operator.equals("*")) {
-            System.out.println("O Resultado da operação é: " + (number1 * number2));                 
-        }
-
     }
-}
