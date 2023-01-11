3bd84356815b75d516551de64d3237a11ceb7a9c
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-01-10 23:52:24 -0300

    Lojinha de doces

--- metodoscargaretorno/src/edu/dio/projetobasico/LojinhaDeDoces.java
+++ metodoscargaretorno/src/edu/dio/projetobasico/LojinhaDeDoces.java
@@ -6,11 +6,8 @@ public class LojinhaDeDoces {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         int doce = leitor.nextInt();
-
-        // TODO: Crie a condição necessária para que cada cliente saiba quantos doces
-        // vai obter de acordo com a entrada
-        int qt_Doces = doce * 2;
-        System.out.println("O cliente obteve " + qt_Doces + " doces");
+        int numberCand = doce * 2;
+        System.out.println("O cliente obteve " + numberCand + " doces");
 
     }
 }
