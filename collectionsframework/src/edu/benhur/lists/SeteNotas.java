b600ccf66abc100cfcd291841073e75725fc0026
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-01-11 11:21:29 -0300

    criando uma nova lista

---
+++ collectionsframework/src/edu/benhur/lists/SeteNotas.java
@@ -0,0 +1,11 @@
+package edu.benhur.lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeteNotas {
+    public static void main(String[] args) {
+        System.out.println("Crie uma lista e adicione as notas: ");
+        List<Double> notas = new ArrayList<>();
+    }
+}
