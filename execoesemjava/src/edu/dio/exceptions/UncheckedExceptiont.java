515f2420a0637db6895d5dbe1810d03e09456e50
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-01-22 11:06:59 -0300

    UncheckedException

--- execoesemjava/src/edu/dio/exceptions/UncheckedExceptiont.java
+++ execoesemjava/src/edu/dio/exceptions/UncheckedExceptiont.java
@@ -7,6 +7,10 @@ public class UncheckedExceptiont {
      * @param args
      */
     public static void main(String[] args) {
+
+        boolean loop =  true;
+
+        do {
         String a = JOptionPane.showInputDialog("Numerador: ");
         String b = JOptionPane.showInputDialog("Denominador: ");
 
@@ -14,14 +18,21 @@ public class UncheckedExceptiont {
    
             int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
             System.out.println("Resultado: " + resultado);
+            loop = false;
         } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(null,
             "Entrada inválida, informe um número inteiro! " + e.getMessage());
+            loop = false;
+        } catch (ArithmeticException e) {
+            JOptionPane.showMessageDialog(null,
+            "Entrada inválida, informe um número maior que zero! " + e.getMessage());
+            loop = false;
         } finally {
 
             System.out.println("Chegou no finally!");
             
         }
+        } while (loop);
         System.out.println("O Código continua...");
 
     }
