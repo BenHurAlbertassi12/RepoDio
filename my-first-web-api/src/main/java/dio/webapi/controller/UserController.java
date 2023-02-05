07341acf1dd51a92eae338ece3235bf5445de7d5
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-02-05 12:35:49 -0300

    Delete e GetUser

--- my-first-web-api/src/main/java/dio/webapi/controller/UserController.java
+++ my-first-web-api/src/main/java/dio/webapi/controller/UserController.java
@@ -3,7 +3,9 @@ package dio.webapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import dio.webapi.model.Usuario;
@@ -15,6 +17,15 @@ public class UserController {
     private UserRepository repository;
     @GetMapping("/users")
     public List<Usuario> getUsers() {
-       return repository.findAll();
+        return repository.findAll();
+    }
+
+    @GetMapping("/users/{username}")
+    public Usuario getOne(@PathVariable("username") String username) {
+        return repository.findByUsername(username);
+    }
+    @DeleteMapping("/users/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        repository.deleteById(id);
     }
 }
